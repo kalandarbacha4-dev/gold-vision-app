baa2b478feccc89878a5849b19c548f0c6e37fb3
kalandarbacha4-dev <kalandarbacha4@gmail.com>
2026-09-20 14:37:40 +0430

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,9 @@
 
+‏const SUPABASE_URL = "https://ajzadjmxtnvzdzlxwtyo.supabase.co/rest/v1/";
+
+‏const SUPABASE_KEY = "sb_publishable_faLpmyuJDEydfsI2mwkETg_27i57I-7";
+
+‏const supabaseClient = window.supabase.createClient(
+‏  SUPABASE_URL,
+‏  SUPABASE_KEY
+);
